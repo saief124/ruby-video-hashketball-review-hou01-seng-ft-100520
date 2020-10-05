6f02057b954cb5c634c4242b8f2b03ebff45b069
Saief I Sayed <saiefsayed123@hotmail.com>
2020-10-05 22:14:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,4 +127,21 @@ def game_hash
   }
 end
 
+def all_players
+  home_players=game_hash[:home][:players]
+  away_players=game_hash[:away][:players]
+  homr_players + away_players
+end
+
+def find_players(player_name)
+  some_player=all_players.find do |player|
+    player[:player_name]==player_name
+  end
+  return some_player
+end
+
+def num_points_scored(player_name)
+  find_players(player_name)[:points]
+end
+
 # Write code here
